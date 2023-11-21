e8490f21756d2b241c9c3ba5f45bcb556bd217d6
Aan Alma <aanalma03@gmail.com>
2023-11-22 00:29:10 +0700

    menambahkan CRUD di RoomController

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -13,6 +13,7 @@ class RoomController extends Controller
     public function index()
     {
         //
+        return view('');
     }
 
     /**
@@ -21,6 +22,7 @@ class RoomController extends Controller
     public function create()
     {
         //
+        return view('');
     }
 
     /**
@@ -29,6 +31,29 @@ class RoomController extends Controller
     public function store(Request $request)
     {
         //
+        $validateData = $request->validate([
+            'room_number' => 'required|max:5',
+            'price' => 'required|max:20',
+            'room_type' => 'required',
+            'image' => 'required||file|image|max:10024',
+            'status' => 'required',
+            'description' => 'required|max:255'
+        ]);
+
+        if($request->file('image')){
+            $validateData['image'] = $request->file('image')->store('file-storage');
+        }
+
+        Room::create([
+            'room_number' => $validateData['room_number'],
+            'price' => $validateData['price'],
+            'room_type' => $validateData['room_type'],
+            'image' => $validateData['image'],
+            'status' => $validateData['status'],
+            'description' => $validateData['description']
+        ]);
+
+        return redirect('')->with('status', 'Room Added !');
     }
 
     /**
@@ -37,6 +62,9 @@ class RoomController extends Controller
     public function show(Room $room)
     {
         //
+        return view('', [
+            'data' => $room
+        ]);
     }
 
     /**
@@ -45,6 +73,9 @@ class RoomController extends Controller
     public function edit(Room $room)
     {
         //
+        return view('', [
+            'data' => $room,
+        ]);
     }
 
     /**
@@ -53,6 +84,32 @@ class RoomController extends Controller
     public function update(Request $request, Room $room)
     {
         //
+        $validateData = $request->validate([
+            'room_number' => 'required|max:5',
+            'price' => 'required|max:20',
+            'room_type' => 'required',
+            'image' => 'required||file|image|max:10024',
+            'status' => 'required',
+            'description' => 'required|max:255'
+        ]);
+
+        if($request->file('image')){
+            if ($room->image && file_exists(storage_path('app/public/' . $room->image))){
+                Storage::delete(['public/'.$room->image]);
+                $validateData['image'] = $request->file('image')->store('file-storage');
+            }
+        }
+
+        $room->update([
+            'room_number' => $validateData['room_number'],
+            'price' => $validateData['price'],
+            'room_type' => $validateData['room_type'],
+            'image' => $validateData['image'],
+            'status' => $validateData['status'],
+            'description' => $validateData['description']
+        ]);
+
+        return redirect('' . $room->id)->with('status', 'Room Updated !');
     }
 
     /**
@@ -61,5 +118,6 @@ class RoomController extends Controller
     public function destroy(Room $room)
     {
         //
+        Room::destroy($room->id);
     }
 }
