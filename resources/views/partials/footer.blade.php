99fb5fbc5dfdeafc9aee08b748339ae4506fe4c0
Fikrianamm <fikritaufiqulanam1@gmail.com>
2023-11-22 10:18:05 +0700

    membuat landing page view

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800 mt-12">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
       <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Sistem Manajemen Kos</a>. All Rights Reserved.
     </span>
