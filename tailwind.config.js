530d39b05fe10ea1a5d7c562089c76c6e9c874f4
Fikrianamm <fikritaufiqulanam1@gmail.com>
2023-11-22 08:50:27 +0700

    install tailwind flowbite

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: "class",
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("flowbite/plugin")],
+};
