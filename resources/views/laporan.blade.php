21155a42bb45f84ffea7e0c765d4699d71705b11
Aditiarzky <aditiarizkyuttama@gmail.com>
2023-11-26 11:00:15 +0700

    tamabah pendapatan dan currency serta beberapa perubahan

--- resources/views/laporan.blade.php
+++ resources/views/laporan.blade.php
@@ -41,6 +41,12 @@
                             <th scope="col" class="px-6 py-3">
                                 Total Pengeluaran
                             </th>
+                            <th scope="col" class="px-6 py-3">
+                                Pendapatan Kotor
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Pendapatan Bersih
+                            </th>
                             <th id="kanan" scope="col" class="px-6 py-3">
                                 Action
                             </th>
@@ -58,26 +64,32 @@
                             <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 01
                             </td>
-                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white currency">
                                 Pending
                             </td>
-                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white currency">
                                 Pending
                             </td>
-                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white currency">
                                 Pending
                             </td>
-                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white currency">
                                 Pending
                             </td>
-                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white currency">
                                 Pending
                             </td>
-                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white currency">
                                 Pending
                             </td>
-                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white currency">
                                 Pending
+                            </td>
+                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white currency">
+                                20000
+                            </td>
+                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white currency">
+                                20000
                             </td>
                             <td class="px-6 py-4">
                                 <a href="#"
