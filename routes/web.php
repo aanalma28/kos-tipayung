14d02f63906541ddbd481324922a2663e25ce444
Fikrianamm <fikritaufiqulanam1@gmail.com>
2023-11-27 16:51:33 +0700

    tambah form input pembayaran, ubah posisi logout

--- routes/web.php
+++ routes/web.php
@@ -48,23 +48,26 @@ Route::get('/pengajuan', function () {
 // url /user -> menampilkan halaman dari semua data user
 // url /user/create -> menampilkan halaman form buat user
 // url /user/{idspesifik} -> menampilkan form halaman edit user jika user di klik
-Route::resource('/user', UserController::class);
+Route::resource('/user', UserController::class)->middleware('owner');
 
 // room route
 // kalo resource handle halaman CRUD dalam satu route
-Route::resource('/room', RoomController::class);
+Route::resource('/room', RoomController::class)->middleware('owner');
 
 
 // register room
 Route::post('/register-room', [RegisterRoomController::class, 'create']);
-Route::get('/room-{room:id}/detail', [RegisterRoomController::class, 'index']);
+Route::get('/register-room/{room:id}/detail', [RegisterRoomController::class, 'index']);
 
 // login route
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
 
 // dashboard controller
-Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('dashboard');
+Route::post('/dashboard/{user:id}/accept', [DashboardController::class, 'accept'])->middleware('owner');
+Route::post('/dashboard/{user:id}/decline', [DashboardController::class, 'decline'])->middleware('owner');
+Route::post('/dashboard/{user:id}/delete', [DashboardController::class, 'delete'])->middleware('owner');
 
 // tabungan controller
 Route::get('/edittabungan',[TabunganController::class, 'store']);
