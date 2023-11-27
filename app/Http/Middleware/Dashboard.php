121ad9bec84696cb116484b12b273ae9f7ead679
Aan Alma <aanalma03@gmail.com>
2023-11-27 11:34:03 +0700

    menambahkan middleware

---
+++ app/Http/Middleware/Dashboard.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Dashboard
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!auth()->check()){
+            abort(403);
+        }
+        return $next($request);
+    }
+}
