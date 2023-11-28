dada10be12564ab4fd6904bb3cc6ab85c1f0b80c
Aan Alma <aanalma03@gmail.com>
2023-11-28 08:09:28 +0700

    memperbaiki form pembayaran

--- app/Http/Controllers/PembayaranController.php
+++ app/Http/Controllers/PembayaranController.php
@@ -9,24 +9,23 @@ class PembayaranController extends Controller
 {
     //
     public function store(Request $request){
+        
         $validatedData = $request->validate([
             'user_id' => 'required',
             'room_id' => 'required',
             'waktu' => 'required',
-            'image' => 'required|file|image|max:10024',
-        ]);
-
-        dd($validateData);
+            'image' => 'required|image|file|max:10024',
+        ]);        
 
         if($request->file('image')){
-            $validateData['image'] = $request->file('image')->store('pembayaran');
+            $validatedData['image'] = $request->file('image')->store('pembayaran');
         }
 
         Pembayaran::create([
-            'user_id' => $validateData['user_id'],
-            'room_id' => $validateData['room_id'],
-            'tanggal_pembayaran' => $validateData['waktu'],
-            'image' => $validateData['image']
+            'user_id' => $validatedData['user_id'],
+            'room_id' => $validatedData['room_id'],
+            'tanggal_pembayaran' => $validatedData['waktu'],
+            'image' => $validatedData['image']
         ]);
 
         return redirect('/dashboard')->with('success', 'Payment evidence has been send !');
