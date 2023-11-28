680c151594e5d324805e9b27885c14ead1d7103f
unknown <m.azis1711@gmail.com>
2023-11-28 13:24:05 +0700

    menambahkan email

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,13 +1,42 @@
 {{-- html yang akan dikirimkan ke email  --}}
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Send Email</title>
 </head>
+
 <body>
     <h3>Halo, {{ $ArrayData['name'] }}</h3>
+
+
+    <body style="margin: 0; padding: 0; font-family: Arial, sans-serif; background-color: #f4f4f4;">
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+            <tr>
+                <td>
+                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;">
+                        <tr>
+                            <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+                                <h2 style="margin-top: 20px; color: #333;">Permintaan telah diterima</h2>
+                                <p style="color: #555;">Tunggu untuk info selanjutnya</p>
+                                <a href="http://127.0.0.1:8000/" style="display: inline-block; padding: 10px 20px; margin-top: 10px; border-radius: 5px; text-decoration: none; color: #fff; background-color: #1db954;">Situs Kami</a>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </body>
+
+    </html>
+
+
+
+    @vite(['resources/css/app.css','resources/js/app.js'])
+
 </body>
+
 </html>
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Managemen Kos | Owner</title>
     <script>
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
         if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
             document.documentElement.classList.add('dark');
         } else {
@@ -14,14 +15,16 @@
     </script>
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
+
 <body class=" bg-white dark:bg-gray-900">
-   @includeWhen(auth()->user()->role === 'penyewa', 'partials.sidebarPenyewa')
-   @includeWhen(auth()->user()->role === 'owner', 'partials.sidebarOwner')
-      <div class="p-4 sm:ml-64 ">
-         <div class="p-4 mt-14">
+    @includeWhen(auth()->user()->role === 'penyewa', 'partials.sidebarPenyewa')
+    @includeWhen(auth()->user()->role === 'owner', 'partials.sidebarOwner')
+    <div class="p-4 sm:ml-64 ">
+        <div class="p-4 mt-14">
             @yield('content')
-         </div>
-      </div>
+        </div>
+    </div>
     @include('partials.footer')
 </body>
+
 </html>
